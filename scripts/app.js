b0b65fd77b5d102e2f27fa52459a938c230c1f14
williamv1 <wmvanhook@gmail.com>
2020-09-22 17:15:48 -0400

    Add updates to buttons, main-screen

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,8 @@
 - hunger increments by 1 every 15 seconds; decrements by one when fed
 - sleepiness increments by 2 every 15 seconds with lights off; decrements by 1 when lights on
 - boredom increments by 3 every 15 seconds; decrements by 1 when played with
-- age increments by 1 every minute alive
+- age increments by 1 every minute alive and it grows
 - Tomagatchi dies when hunger, sleepiness, or boredom reaches 10
-- when Tomagatchi age increments by 2, it grows
 
 - to implement timer => use set interval 
 https://stackoverflow.com/questions/21638574/run-a-function-every-30-seconds-javascript/49237391
@@ -27,38 +26,23 @@ class Tomagatchi {
     this.age = age;
   }
   timer() {
+    //set up metrics and time to increment
     let time = 0;
     let hunger = 0;
     let boredom = 0;
     let sleepiness = 0;
-    const timeCount = document.getElementById('time');
-    const hungerCount = document.getElementById('hunger');
-    const boredomCount = document.getElementById('boredom');
-    const sleepCount = document.getElementById('sleep');
-    // const increaseHunger = () => {
-        
-    //     console.log(`${this.name}'s hunger increased by one`);
-
-    // };
-
-    // const increaseBoredom = () => {
-    //     console.log(`${this.name}'s hunger increased by three`);
-    // };
-
-    // const increaseSleepiness = () => {
-    //     console.log(`${this.name}'s hunger increased by two`);
-    // };
+    const timeCount = document.getElementById("time");
+    const hungerCount = document.getElementById("hunger");
+    const boredomCount = document.getElementById("boredom");
+    const sleepCount = document.getElementById("sleep");
 
     const timer = setInterval(function () {
-        // increaseHunger();
-        // increaseBoredom();
-        // increaseSleepiness();
-        console.log(`${time} seconds elasped`);
-        timeCount.textContent = `${time += 3} seconds`;
-        hungerCount.textContent = hunger += 1;
-        boredomCount.textContent = boredom += 3;
-        sleepCount.textContent = sleepiness += 2;
-        // time += 15;
+      console.log(`${time} seconds elasped`);
+      timeCount.textContent = `${(time += 3)} seconds`;
+      hungerCount.textContent = hunger += 1;
+      boredomCount.textContent = boredom += 3;
+      sleepCount.textContent = sleepiness += 2;
+
     }, 3000);
   }
   feed() {
@@ -77,14 +61,7 @@ class Tomagatchi {
 
 let figure = new Tomagatchi("Furball", 0, 0, 0, 0);
 
-// figure.timer()
-//Display metrics for pet
-
-// Form
-
-//Decrease hunger, boredom and sleepiness
-//Add buttons for functions
-
+//transition between login and welcome screens
 
 submit.addEventListener("click", (e) => {
   const loginScreen = document.querySelector(".login");
@@ -97,19 +74,49 @@ submit.addEventListener("click", (e) => {
   welcome.innerHTML = `<h2>Welcome ${userName}!</h2>`;
 });
 
-petName.addEventListener("keypress", (e) => {
-    console.log(e.key)
-    if(e.key === 'Enter'){
-    const mainScreen = document.getElementById('main-screen');
-    const petGreeting = document.querySelector('.day h2')
-    const pet = document.getElementById('pet-name').value;
+/*transition between welcome and main screen;
+display Tomagatchi figure
+start timer on main screen
+*/
 
-  welcomeScreen.style.display = "none";
-  mainScreen.style.display = "block";
-  petGreeting.innerHTML = `${pet}`
-  figure.timer();
+petName.addEventListener("keypress", (e) => {
+  console.log(e.key);
+  if (e.key === "Enter") {
+    const mainScreen = document.getElementById("main-screen");
+    const petGreeting = document.querySelector(".day h2");
+    const buttons = document.querySelector(".buttons");
+    const pet = document.getElementById("pet-name").value;
+    let figureChoice = document.querySelector("#figure").value;
+    let panda = document
+      .getElementsByTagName("option")[0]
+      .getAttribute("value");
+    let hamster = document
+      .getElementsByTagName("option")[1]
+      .getAttribute("value");
+    let monkey = document
+      .getElementsByTagName("option")[2]
+      .getAttribute("value");
+    let figureDisplay = document.createElement("div");
+
+    welcomeScreen.style.display = "none";
+    mainScreen.style.display = "block";
+    petGreeting.innerHTML = `${pet}`;
+
+    if (figureChoice === panda) {
+      figureDisplay.innerHTML = '<img src="images/panda.png">';
+      buttons.prepend(figureDisplay);
+    } else if (figureChoice === hamster) {
+      figureDisplay.innerHTML = '<img src="images/hamster.png">';
+      buttons.prepend(figureDisplay);
+    } else if (figureChoice === monkey) {
+      figureDisplay.innerHTML = '<img src="images/monkey.png">';
+      buttons.prepend(figureDisplay);
     }
+    figure.timer();
+  }
 });
+
+//Decrease hunger, boredom and sleepiness
 
 feed.addEventListener("click", (e) => {
   figure.feed();
@@ -123,6 +130,15 @@ sleep.addEventListener("click", (e) => {
   figure.wake();
 });
 
-light.addEventListener("click", (e) => {
-  dayTime.classList.toggle("night");
+/*Change from light to dark and vice versa; consulted code snippet for toggle from 
+https://www.w3schools.com/howto/howto_js_toggle_text.asp*/
+
+light.addEventListener('click', (e) => {
+    let lightId = document.getElementById('light');
+    dayTime.classList.toggle("night");
+    if (lightId.textContent === 'Turn Off Light'){
+        lightId.textContent = 'Turn On Light'
+    } else {
+        lightId.textContent = 'Turn Off Light'
+    }
 });
