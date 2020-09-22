177a261aa54b05166ef7a4c9f5013dd4504ed055
williamv1 <wmvanhook@gmail.com>
2020-09-22 14:14:58 -0400

    Update CSS and divs functionaity

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,3 @@
-//Add buttons for functions
-
 /*Create a Tomagatchi class and instatiate class (hunger, sleepiness, boredom, age);
 - hunger increments by 1 every 15 seconds; decrements by one when fed
 - sleepiness increments by 2 every 15 seconds with lights off; decrements by 1 when lights on
@@ -13,82 +11,103 @@ https://stackoverflow.com/questions/21638574/run-a-function-every-30-seconds-jav
 */
 
 class Tomagatchi {
-    constructor(name, hunger, sleepiness, boredom, age){
-        this.name = name;
-        this.hunger = hunger;
-        this.sleepiness = sleepiness;
-        this.boredom = boredom;
-        this.age = age;
-    }
-    timer() {
-        let time = 0;
+  constructor(name, hunger, sleepiness, boredom, age) {
+    this.name = name;
+    this.hunger = hunger;
+    this.sleepiness = sleepiness;
+    this.boredom = boredom;
+    this.age = age;
+  }
+  timer() {
+    let time = 0;
 
-        const increaseHunger = () => {
-            console.log(`${this.name}'s hunger increased by one`)
-        }; 
+    const increaseHunger = () => {
+      console.log(`${this.name}'s hunger increased by one`);
+    };
 
-        const increaseBoredom = () => {
-            console.log(`${this.name}'s hunger increased by three`)
-        }; 
+    const increaseBoredom = () => {
+      console.log(`${this.name}'s hunger increased by three`);
+    };
 
-        const increaseSleepiness = () => {
-            console.log(`${this.name}'s hunger increased by two`)
-        }; 
+    const increaseSleepiness = () => {
+      console.log(`${this.name}'s hunger increased by two`);
+    };
 
-        const timer = setInterval(function () {
-            increaseHunger();
-            increaseBoredom();
-            increaseSleepiness()
-            console.log(`${time} seconds elasped`)
-            time+= 15
-        }, 15000)
+    const timer = setInterval(function () {
+      increaseHunger();
+      increaseBoredom();
+      increaseSleepiness();
+      console.log(`${time} seconds elasped`);
+      time += 15;
+    }, 15000);
+  }
+  feed() {
+    console.log(`${this.name}'s hunger decreased by one`);
+  }
+  wake() {
+    console.log(`${this.name}'s sleepiness decreased by one`);
+  }
+  play() {
+    console.log(`${this.name}'s boredom decreased by one`);
+  }
+  // age(){
 
-    }
-    feed () {
-        console.log(`${this.name}'s hunger decreased by one`)        
+  // }
+}
 
-    }
-    wake(){
-        console.log(`${this.name}'s sleepiness decreased by one`) 
-    }
-    play(){
-        console.log(`${this.name}'s boredom decreased by one`)  
-    }
-    // age(){
+let figure = new Tomagatchi("Furball", 0, 0, 0, 0);
 
-    // }
-};
-
-    let figure = new Tomagatchi('Furball', 0, 0, 0, 0)
-
-    // figure.timer()
+// figure.timer()
 //Display metrics for pet
 
-
-// Form 
+// Form
 
 //Decrease hunger, boredom and sleepiness
+//Add buttons for functions
+const submit = document.getElementById("submit");
+const petName = document.getElementById("pet-name");
+const feed = document.getElementById("feed");
+const play = document.getElementById("play");
+const sleep = document.getElementById("sleep");
+const light = document.getElementById("light");
+const dayTime = document.querySelector(".day");
+const welcomeScreen = document.querySelector(".welcome");
 
+submit.addEventListener("click", (e) => {
+  const loginScreen = document.querySelector(".login");
+  const welcome = document.querySelector(".welcome h2");
+  const userName = document.getElementById("username").value;
 
-const feed = document.getElementById('feed');
-const play = document.getElementById('play');
-const sleep = document.getElementById('sleep');
-const light = document.getElementById('light');
-const dayTime = document.querySelector('.day');
-
-
-feed.addEventListener('click', (e) => {
-    figure.feed()
+  e.preventDefault();
+  loginScreen.style.display = "none";
+  welcomeScreen.style.display = "block";
+  welcome.innerHTML = `<h2>Welcome ${userName}!</h2>`;
 });
 
-play.addEventListener('click', (e) => {
-    figure.play()
+petName.addEventListener("keydown", (e) => {
+    if(e === '13'){
+    const mainScreen = document.getElementById('main-screen');
+    const petGreeting = document.querySelector('.day h2')
+    const pet = document.getElementById('pet-name').value;
+
+  welcomeScreen.style.display = "none";
+  mainScreen.style.display = "block";
+  petGreeting.innerHTML = `${pet}`
+    }
 });
 
-sleep.addEventListener('click', (e) => {
-    figure.wake()
-})
+feed.addEventListener("click", (e) => {
+  figure.feed();
+});
 
-light.addEventListener('click',(e) => {
-    dayTime.classList.toggle('night')
+play.addEventListener("click", (e) => {
+  figure.play();
+});
+
+sleep.addEventListener("click", (e) => {
+  figure.wake();
+});
+
+light.addEventListener("click", (e) => {
+  dayTime.classList.toggle("night");
 });
